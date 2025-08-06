eeae89d20d6b796100ea017edb406d171ec53397
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2025-08-06 17:08:35 -0400

    Update camera.h

--- src/camera.h
+++ src/camera.h
@@ -1,108 +1,54 @@
-#ifndef VEC4
-#define VEC4
-
+#ifndef CAMERA_H
+#define CAMERA_H
 #include <cmath>
 #include <iostream>
+#include "vec3.h"
+#include "mat4.h"
 
-class Vec4 {
-public:
-    double vec[4];
+class camera{
+    public:
+        Vec3 position;
+        Vec3 forward;
+        Vec3 up;
+        Vec3 target;
 
-    Vec4() : vec{0,0,0,0} {}
-    Vec4(double v0, double v1, double v2, double v3) : vec{v0, v1, v2, v3} {}
+        double azimuth;
+        double elevation;
+        double radius;
 
-    double x() const { return vec[0]; }
-    double y() const { return vec[1]; }
-    double z() const { return vec[2]; }
-    double w() const { return vec[3]; }
 
-    Vec4 operator-() const {
-        return Vec4(-vec[0], -vec[1], -vec[2], -vec[3]);
-    }
-
-    double& operator[](int i) { return vec[i]; }
-    const double& operator[](int i) const { return vec[i]; }
-
-    Vec4& operator+=(const Vec4 &v) {
-        for (int i = 0; i < 4; ++i) vec[i] += v.vec[i];
-        return *this;
-    }
-
-    Vec4& operator*=(double t) {
-        for (int i = 0; i < 4; ++i) vec[i] *= t;
-        return *this;
-    }
-
-    Vec4& operator/=(double t) {
-        for (int i = 0; i < 4; ++i) vec[i] /= t;
-        return *this;
-    }
-
-    double length() const {
-        double sum = 0.0;
-        for (int i = 0; i < 4; ++i) sum += vec[i] * vec[i];
-        return std::sqrt(sum);
-    }
-
-    Vec4 getVec() const {
-        for (int i = 0; i < 4; ++i) {
-            if (std::abs(vec[i]) == INFINITY) {
-                std::cerr << "Vector component is infinity!" << std::endl;
-                return Vec4(0.0, 0.0, 0.0, 0.0);
-            }
+        camera() {
+            position = Vec3(0, 0, 5);
+            target = Vec3(0, 0, 0);
+            up = Vec3(0, 1, 0);
+            azimuth = 0.0;
+            elevation = 0.0;
+            radius = 5.0;
         }
-        return *this;
-    }
+
+
+        Vec3 updatePosition(){
+            double x = x = radius * cos(elevation) * sin(azimuth);
+            double y = radius * sin(elevation);
+            double z = radius * cos(elevation) * cos(azimuth);
+
+            position = {x,y,z};
+            return position;
+
+        }
+
+        mat4 lookAt(Vec3 position, Vec3 target, Vec3 up){
+            Vec3 forward = unit_vector(target - position);
+            Vec3 right = unit_vector(cross(forward, up));
+            up = cross(right, forward);
+
+            mat4 rot = mat4::rotationMat(right, up, forward);
+            mat4 tran = mat4::translation(-position.x(), -position.y(), -position.z());
+
+            return rot * tran;
+        }
+
+
 };
-
-// Non-member operators
-
-inline Vec4 operator*(const Vec4 &u, const Vec4 &v) {
-    return Vec4(u[0] * v[0], u[1] * v[1], u[2] * v[2], u[3] * v[3]);
-}
-
-inline Vec4 operator*(double t, const Vec4 &v) {
-    return Vec4(t * v[0], t * v[1], t * v[2], t * v[3]);
-}
-
-inline Vec4 operator*(const Vec4 &v, double t) {
-    return t * v;
-}
-
-inline Vec4 operator/(const Vec4& v, double t) {
-    return Vec4(v[0] / t, v[1] / t, v[2] / t, v[3] / t);
-}
-
-inline Vec4 operator+(const Vec4& a, const Vec4& b) {
-    return Vec4(a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]);
-}
-
-inline Vec4 operator-(const Vec4& a, const Vec4& b) {
-    return Vec4(a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]);
-}
-
-inline Vec4 operator-(const Vec4& v, double x) {
-    return Vec4(v[0] - x, v[1] - x, v[2] - x, v[3] - x);
-}
-
-// Unit vector
-inline Vec4 unit_vector(Vec4 v) {
-    double len = v.length();
-    if (len != 0.0) {
-        return v / len;
-    } else {
-        std::cerr << "Cannot calculate unit vector for zero-length vector." << std::endl;
-        return Vec4(0, 0, 0, 0);
-    }
-}
-
-// Dot product (all 4 components)
-inline double dot(const Vec4 &u, const Vec4 &v) {
-    double result = 0.0;
-    for (int i = 0; i < 4; ++i) {
-        result += u.vec[i] * v.vec[i];
-    }
-    return result;
-}
 
 #endif
